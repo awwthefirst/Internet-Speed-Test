f1d8d5b17d793139eb9560871a2f78e3bd8fe188
Thomas <thomas.vegar@gmail.com>
2021-06-09 11:31:02 +1200

    Refactored to use global HotKeys to detect when alt + is pressed.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Timers;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace Internet_Speed_Test
 {
@@ -21,42 +23,69 @@ namespace Internet_Speed_Test
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int HOTKEY_ID = 9000;
+        private const uint MOD_NONE = 0x0000; //[NONE]
+        private const uint MOD_ALT = 0x0001; //ALT
+        private const uint MOD_CONTROL = 0x0002; //CTRL
+        private const uint MOD_SHIFT = 0x0004; //SHIFT
+        private const uint MOD_WIN = 0x0008; //WINDOWS
+                                             //CAPS LOCK:
+        private const uint VK_CAPITAL = 0x49;
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        private HwndSource source;
 
         private bool isVisible = false;
-        private int cooldown = 0;
 
         public MainWindow()
         {
             InitializeComponent();
-            this.Hide();
-            Timer timer = new Timer(10);
-            timer.Elapsed += CheckIfKeysPressed;
-            timer.Start();
         }
 
-        private void CheckIfKeysPressed(Object source, ElapsedEventArgs e) //Maybe change keybinds
+        protected override void OnSourceInitialized(EventArgs e)
         {
-            if (cooldown > 0)
+            base.OnSourceInitialized(e);
+
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+
+            source = HwndSource.FromHwnd(handle);
+            source.AddHook(this.HwndHook);
+
+            RegisterHotKey(handle, HOTKEY_ID, MOD_ALT, VK_CAPITAL);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
             {
-                cooldown--;
-            }
-            else
-            {
-                Application.Current.Dispatcher.Invoke((Action)delegate
-                {
-                    bool altPressed = Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt);
-                    bool iPressed = Keyboard.IsKeyDown(Key.I);
-                    if (altPressed && iPressed)
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
                     {
-                        if (this.isVisible)
-                            this.Hide();
-                        else
-                            this.Show();
-                        this.isVisible = !this.isVisible;
-                        cooldown = 30;
+                        case HOTKEY_ID:
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_CAPITAL)
+                            {
+                                this.ToggleVisibility();
+                            }
+                            handled = true;
+                            break;
                     }
-                });
+                    break;
             }
+            return IntPtr.Zero;
+        }
+
+        private void ToggleVisibility()
+        {
+            if (!this.isVisible)
+                this.Show();
+            else
+                this.Hide();
+            this.isVisible = !this.isVisible;
         }
     }
 }
