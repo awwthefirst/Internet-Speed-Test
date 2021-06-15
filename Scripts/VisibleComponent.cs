7deaabbf32710f2be881e8818dcd0e974fec1467
Thomas <thomas.vegar@gmail.com>
2021-06-16 11:33:49 +1200

    Added the first iteration of ping component.

---
+++ Scripts/VisibleComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_Speed_Test.Scripts
+{
+    public abstract class VisibleComponent
+    {
+        public abstract void OnSetVisible(MainWindow mainWindow);
+        public abstract void OnSetHidden(MainWindow mainWindow);
+    }
+}
