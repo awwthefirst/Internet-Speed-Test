fc423445a8f600803d458bd0609ee03f066d51eb
Thomas <thomas.vegar@gmail.com>
2021-06-22 15:19:02 +1200

    Commented the ping component

--- Scripts/PingComponent.cs
+++ Scripts/PingComponent.cs
@@ -8,18 +8,18 @@ namespace Internet_Speed_Test.Scripts
 {
     public class PingComponent : VisibleComponent
     {
-        private Timer currentTimer;
-        private List<long> previousResults;
+        private Timer currentTimer; //A timer to call UpdatePing() once a second
+        private List<long> previousResults; //Used for getting a average of results to smooth them out
         public override void OnSetVisible(MainWindow mainWindow)
         {
             this.previousResults = new List<long>();
 
-            this.currentTimer = new Timer(1000);
+            this.currentTimer = new Timer(1000); //Setups the timer
             this.currentTimer.Elapsed += (sender, e) => this.UpdatePing(sender, e, mainWindow);
             this.currentTimer.Start();
         }
 
-        private void UpdatePing(object source, ElapsedEventArgs e, MainWindow mainWindow)
+        private void UpdatePing(object source, ElapsedEventArgs e, MainWindow mainWindow) //Called once a second by the currentTimer
         {
             Application.Current.Dispatcher.Invoke(delegate
             {
@@ -28,9 +28,8 @@ namespace Internet_Speed_Test.Scripts
                     this.previousResults.Remove(0);
                 }
                 long result = this.TestPing();
-                Console.WriteLine(result);
 
-                if (result == -1)
+                if (result == -1) //If you have no internet
                 {
                     mainWindow.PingText.Content = "Disconected";
                 } else
@@ -38,13 +37,15 @@ namespace Internet_Speed_Test.Scripts
                     this.previousResults.Add(result);
                     long combinedResults = 0;
                     this.previousResults.ForEach(l => { combinedResults += l; });
-                    long averageResult = combinedResults / this.previousResults.Count;
+                    long averageResult = combinedResults / this.previousResults.Count; //Average of the past 3 calls
 
-                    mainWindow.PingText.Content = averageResult + "ms";
+                    mainWindow.PingText.Content = averageResult + "ms"; //Sets the label's text
                 }
             });
         }
 
+        ///<summary>Returns the ping roundtrip time to the 1.1.1.1(cloudflare).</summary>
+        ///<returns>The roundtrip time or -1 if it was not successful.</returns>
         private long TestPing()
         {
             long result;
@@ -69,7 +70,7 @@ namespace Internet_Speed_Test.Scripts
         {
             if (this.currentTimer != null)
             {
-                this.currentTimer.Stop();
+                this.currentTimer.Stop(); //Stops the timer
             }
         }
     }
--- Scripts/VisibleComponent.cs
+++ Scripts/VisibleComponent.cs
@@ -8,7 +8,9 @@ namespace Internet_Speed_Test.Scripts
 {
     public abstract class VisibleComponent
     {
+        ///<summary>Called by <c>MainWindow</c> when the window is set visible.</summary>
         public abstract void OnSetVisible(MainWindow mainWindow);
+        ///<summary>Called by <c>MainWindow</c> when the window is set hidden.</summary>
         public abstract void OnSetHidden(MainWindow mainWindow);
     }
 }
